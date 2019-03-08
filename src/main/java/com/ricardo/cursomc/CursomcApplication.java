468c6d72135c4d91c432c62d57a9b6e9a4cf760c
Ricardo R. Paixão <rp_mira@hotmail.com>
2019-03-07 23:23:08 -0300

    Estado e cidade

--- src/main/java/com/ricardo/cursomc/CursomcApplication.java
+++ src/main/java/com/ricardo/cursomc/CursomcApplication.java
@@ -8,18 +8,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ricardo.cursomc.domain.Categoria;
+import com.ricardo.cursomc.domain.Cidade;
+import com.ricardo.cursomc.domain.Estado;
 import com.ricardo.cursomc.domain.Produto;
 import com.ricardo.cursomc.repositories.CategoriaRepository;
+import com.ricardo.cursomc.repositories.CidadeRepository;
+import com.ricardo.cursomc.repositories.EstadoRepository;
 import com.ricardo.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
+	// Nossas dependências
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
 	@Autowired
 	private ProdutoRepository produtoRepositoy;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
 	
 	// inicio p/ rodar aplicação
 	public static void main(String[] args) {
@@ -30,29 +39,56 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		// Cria as Categorias
-		Categoria cat1 = new Categoria(null, "Informática");
-		Categoria cat2 = new Categoria(null, "Escritório");
+		// --- Inicio ---
 		
-		// Cria os Produtos
-		Produto p1 = new Produto(null, "Computador", 2000.00);
-		Produto p2 = new Produto(null, "Impressora", 800.00);
-		Produto p3 = new Produto(null, "Mouse", 80.00);
+			// Cria as Categorias
+			Categoria cat1 = new Categoria(null, "Informática");
+			Categoria cat2 = new Categoria(null, "Escritório");
+			
+			// Cria os Produtos
+			Produto p1 = new Produto(null, "Computador", 2000.00);
+			Produto p2 = new Produto(null, "Impressora", 800.00);
+			Produto p3 = new Produto(null, "Mouse", 80.00);
+			
+			// Associa Categorias aos Produtos
+			cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+			cat2.getProdutos().addAll(Arrays.asList(p2));
+			
+			// Associa Produtos as Categorias
+			p1.getCategorias().addAll(Arrays.asList(cat1));
+			p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+			p3.getCategorias().addAll(Arrays.asList(cat1));
 		
-		// Associa Categorias aos Produtos
-		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
-		cat2.getProdutos().addAll(Arrays.asList(p2));
+			// Manda salvar as Categorias
+			categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+			
+			// Manda salvar os Produtos
+			produtoRepositoy.saveAll(Arrays.asList(p1, p2, p3));
+			
+		// --- FIM ---
 		
-		// Associa Produtos as Categorias
-		p1.getCategorias().addAll(Arrays.asList(cat1));
-		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
-		p3.getCategorias().addAll(Arrays.asList(cat1));
+		// --- Inicio ---
 		
-		// Manda salvar as Categorias
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+			// Cria os Estados
+			Estado est1 = new Estado(null, "Minas Gerais");
+			Estado est2 = new Estado(null, "São Paulo");
+			
+			// Cria as Cidades e associa os Estados
+			Cidade c1 = new Cidade(null, "Uberlândia", est1);
+			Cidade c2 = new Cidade(null, "São Paulo", est2);
+			Cidade c3 = new Cidade(null, "Campinas", est2);
+			
+			// Associa Estado a cidade
+			est1.getCidades().addAll(Arrays.asList(c1));
+			est2.getCidades().addAll(Arrays.asList(c2, c3));
+				
+			// Manda Salvar Estado, respeitar dependencia estado não depende de cidade
+			estadoRepository.saveAll(Arrays.asList(est1, est2));
+			
+			// Manda Salvar Cidade, depende de um estado
+			cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
-		// Manda salvar os Produtos
-		produtoRepositoy.saveAll(Arrays.asList(p1, p2, p3));
+		// --- FIM ---
 		
 	}
 
---
+++ src/main/java/com/ricardo/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,10 @@
+package com.ricardo.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.ricardo.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+	// inteface que faz serviços crud p/ Cidade
+}
