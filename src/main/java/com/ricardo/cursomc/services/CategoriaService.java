9bb7dad6b2b3ba584603b90755ea7564fa74b2c9
Ricardo R. Paixão <rp_mira@hotmail.com>
2019-03-11 20:10:59 -0300

    Listando todas categorias com DTO

--- src/main/java/com/ricardo/cursomc/services/CategoriaService.java
+++ src/main/java/com/ricardo/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.ricardo.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,4 +42,9 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
 		}
 	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
+	
 }
