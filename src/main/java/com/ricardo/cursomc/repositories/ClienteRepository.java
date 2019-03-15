f35ade87c1ce1a30a6bf9e7c3f9f15229b74eb63
Ricardo R. Paixão <rp_mira@hotmail.com>
2019-03-14 23:11:47 -0300

    validacao customizada: email nao repetido na insercao de cliente

--- src/main/java/com/ricardo/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/ricardo/cursomc/repositories/ClienteRepository.java
@@ -2,9 +2,15 @@ package com.ricardo.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.ricardo.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+	
 	// inteface que faz serviços crud p/ Cliente
+	
+	@Transactional(readOnly=true)	// ñ faz uma trasação de BD, faz a consulta ser mais rapida
+	Cliente findByEmail(String email);	// busca um cliente por email
 }
--- src/main/java/com/ricardo/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/ricardo/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ricardo.cursomc.domain.Cliente;
 import com.ricardo.cursomc.domain.enums.TipoCliente;
 import com.ricardo.cursomc.dto.ClienteNewDTO;
+import com.ricardo.cursomc.repositories.ClienteRepository;
 import com.ricardo.cursomc.resources.exception.FieldMessage;
 import com.ricardo.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
 	
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -21,12 +28,20 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
 		
-		// Inclua os teste aqui, inserindo erros na lista
+		// Chama validação do cpf, caso precise vai mandar Msg de invalid 
 		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CPF invalido"));
 		}
+		// Chama validação do cnpj, caso precise vai mandar Msg de invalid
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ invalido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail()); // faz uma busca no bd e retorna um cliente
+		
+		// Se email já existir, emite Msg
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email ja existente"));
 		}
 		
 		for(FieldMessage e : list) {
